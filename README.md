8b4961b22c5c07bc8d35661d3605b57425d493af
romeelkhan <romeel_k@yahoo.co.uk>
2021-12-21 22:53:47 +0000

    Added file extension validation to function

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 A Python cloud function that watermarks an uploaded pdf to cloud storage
 
-# pre-requesites
+## pre-requesites
 
 To use this repo you will require:
 * Python 3
 * gcloud cli
 
-# creating a cloud function from gcloud
+## creating a cloud function from gcloud
 
 Before you create a gcloud function in GCP make sure you have created a GCP project.
 To set the current project to deploy to use the following command:
@@ -22,7 +22,7 @@ To set your desired project run the command:
 gcloud config set project project-name
 ```
 
-# Deploying the sample function
+## Deploying the sample function
 
 To deploy the function run the following gcloud function from the folder where the function resides:
 
@@ -33,3 +33,10 @@ gcloud functions deploy watermark_file \
 ```
 
 Where storagebucket is the bucket triggering the function.
+
+## Uploading a file to test trigger
+
+To test trigger upload a pdf file from this repo:
+
+gsutil cp watermark.pdf  gs://bucketname
+
--- src/function/main.py
+++ src/function/main.py
@@ -13,19 +13,28 @@ def watermark_file(event, context):
         None; the output is written to Stackdriver Logging
     """
     print('instantiating storage client')
+    uploaded_file = format(event['name'])
     client = storage.Client()
     print('Reading from Bucket: {}'.format(event['bucket']))
     bucket = client.get_bucket(event['bucket'])
     print('Reading the file to watermark: {}'.format(event['name']))
+   
+    if(not uploaded_file.endswith('.pdf')):
+        print('Invalid file format uploaded..Function will not watermark')
+        return
+
     blob = bucket.blob(event['name'])
     
-    #print_function_meta_data()
+    print_function_meta_data(context, event)
     
-# def print_function_meta_data(context,event):
-#     print('Event ID: {}'.format(context.event_id))
-#     print('Event type: {}'.format(context.event_type))
+def print_function_meta_data(context, event):
+    print('Event ID: {}'.format(context.event_id))
+    print('Event type: {}'.format(context.event_type))
 
-#     print('File: {}'.format(event['name']))
-#     print('Metageneration: {}'.format(event['metageneration']))
-#     print('Created: {}'.format(event['timeCreated']))
-#     print('Updated: {}'.format(event['updated']))
+    print('File: {}'.format(event['name']))
+    print('Metageneration: {}'.format(event['metageneration']))
+    print('Created: {}'.format(event['timeCreated']))
+    print('Updated: {}'.format(event['updated']))
+
+def watermark_pdf():
+    print('About to watermark pdf...')
