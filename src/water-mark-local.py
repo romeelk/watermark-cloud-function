b5df5dbcfcf61bae207604cf3f1c7790817c49c4
romeelkhan <romeel_k@yahoo.co.uk>
2022-03-09 22:13:55 +0000

    refactored local watermark func

--- src/water-mark-local.py
+++ src/water-mark-local.py
@@ -20,6 +20,10 @@ def generate_watermark(input_pdf_path, watermark_pdf_path):
     watermark_page = watermark_pdf.getPage(0)
     merged = os.path.abspath(input_pdf_path).split('.')[0] +"watermark.pdf"
     #using python library watermark file and write to output stream and close
+    
+    create_watermarked_file(input_pdf, watermark_page, merged)
+
+def create_watermarked_file(input_pdf, watermark_page, merged):
     print(f'Generating watermark file" {merged}')
     output = PdfFileWriter()
 
