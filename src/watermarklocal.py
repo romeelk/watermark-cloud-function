203636bb89a4d29daa3d589662c5ebf56dc088d2
Romeel Khan <romeelkhan@Hiras-MacBook-Air.local>
2022-05-14 11:27:19 +0100

    Added with statements.

--- src/watermarklocal.py
+++ src/watermarklocal.py
@@ -4,24 +4,25 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 
 import os
 import sys
+import errno
+import os
 
 def generate_watermark(input_pdf_path, watermark_pdf_path):
     # validate if both files exist otherwise throw error
-    if not os.path.exists(input_pdf_path):
-        raise FileNotFoundError
-    
-    if not os.path.exists(watermark_pdf_path):
-        raise FileNotFoundError
     
     pdf_writer = PdfFileWriter()
     
-    input_pdf = PdfFileReader(input_pdf_path)
-    watermark_pdf = PdfFileReader(watermark_pdf_path)
-    watermark_page = watermark_pdf.getPage(0)
-    merged = os.path.abspath(input_pdf_path).split('.')[0] +"watermark.pdf"
-    #using python library watermark file and write to output stream and close
+    with open(input_pdf_path, "rb") as input_pdf_stream:
+        input_pdf = PdfFileReader(input_pdf_stream)
+
+        with open(watermark_pdf_path, "rb") as watermark_pdf_stream:
+            watermark_pdf = PdfFileReader(watermark_pdf_stream)
+            
+            watermark_page = watermark_pdf.getPage(0)
+            merged = os.path.abspath(input_pdf_path)[:-4] +"_watermark.pdf"
+            #using python library watermark file and write to output stream and close
     
-    create_watermarked_file(input_pdf, watermark_page, merged)
+            create_watermarked_file(input_pdf, watermark_page, merged)
 
 def create_watermarked_file(input_pdf, watermark_page, merged):
     print(f'Generating watermark file" {merged}')
@@ -35,16 +36,24 @@ def create_watermarked_file(input_pdf, watermark_page, merged):
     with open(merged, "wb") as merged_file:
         output.write(merged_file)
 
-    #Append the suffix watermark to input file name
-def format_file(path):
-    return f"{path}_watermark.pdf"
+#Append the suffix watermark to input file name
     
 if __name__ == "__main__":
     print("Enter pdf to watermark:")
     inputpdf = input()
+
+    if not inputpdf.endswith(".pdf"):
+        print("Please supply a valid pdf file with correct extension!!")
+        sys.exit(-1)
+
     print("Enter watermark pdf:")
     watermark_pdf = input()
     
+    if not os.path.exists(os.path.abspath(inputpdf)):
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), inputpdf)
+    
+    if not os.path.exists(os.path.abspath(watermark_pdf)):
+         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), watermark_pdf)
     try:
         generate_watermark(os.path.abspath(inputpdf), os.path.abspath(watermark_pdf))
     except:
